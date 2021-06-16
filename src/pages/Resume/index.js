69d5681bcc6230396bfc72785dd4661c51ad5bcc
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-16 14:00:44 -0500

    trying to fix routing issue

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import resume from '../../assets/NickolausCarterResume.pdf'
+import React from 'react';
+import resume from '../../assets/NickolausCarterResume.pdf';
 import '../../assets/css/resume.css';
 
 
